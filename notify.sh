d3359882b429a6e06c18a98bcdbc38bd5a3b6186
Pranav Devarakonda <devarakondapranav@yahoo.com>
2018-10-29 00:20:24 +0530

    changed notify.sh to alert based on limit set by user

--- notify.sh
+++ notify.sh
@@ -1,10 +1,12 @@
-free_memory=$1
+free_memory=$(free -m | awk '{print $7}')
+free_memory=$(($free_memory/1024)).$(($free_memory%1024))
 
-mem_less_than_1=$(bc <<< "$free_memory < 1")
+limit=$1
+mem_less_than_1=$(bc <<< "$free_memory < $limit")
 mem_less_than_pt5=$(bc <<< "$free_memory < 0.5")
 
 if [ $mem_less_than_pt5 -eq 1 ]; then
     notify-send "Less than 0.5GiB of memory is free."
 elif [ $mem_less_than_1 -eq 1 ]; then
-    notify-send "Less than 1GiB of memory is free."
+    notify-send "Less than $limit GB of memory is free."
 fi
